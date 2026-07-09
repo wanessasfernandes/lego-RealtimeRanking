bca4e1adb17ca22d8d6cb4dfbd66691d6245265b
Wanessa Fernandes <wanessadesouzafernandesvr@gmail.com>
2026-07-09 12:41:10 -0300

    teste 2

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -2,6 +2,8 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings): 
     app_name: str = "Lego Realtime Ranking"
-    cors_origins: list[str] = ["http://localhost:5173"]
+    cors_origins: list[str] = [
+        "http://localhost:5173",
+        "https://lego-realtime-ranking.vercel.app"]
 
 Settings = Settings()
